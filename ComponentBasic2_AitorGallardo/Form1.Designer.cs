ea5198e501f45ce6849679e245bf0b583448ff17
aitorgit <tugasmec@gmail.com>
2018-12-03 16:56:09 +0100

    first

---
+++ ComponentBasic2_AitorGallardo/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace ComponentBasic2_AitorGallardo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.comma = new System.Windows.Forms.Button();
+            this.back = new System.Windows.Forms.Button();
+            this.deleteAll = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.newCoin = new System.Windows.Forms.Button();
+            this.deleteCoin = new System.Windows.Forms.Button();
+            this.Convert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(89, 191);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(143, 191);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 25);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(197, 191);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(50, 25);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(89, 153);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(50, 25);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(143, 153);
+            this.button5.Margin = new System.Windows.Forms.Padding(2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(50, 25);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(197, 153);
+            this.button6.Margin = new System.Windows.Forms.Padding(2);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(50, 25);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(89, 75);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(193, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(89, 118);
+            this.button7.Margin = new System.Windows.Forms.Padding(2);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(50, 25);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(143, 118);
+            this.button8.Margin = new System.Windows.Forms.Padding(2);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(50, 25);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(197, 118);
+            this.button9.Margin = new System.Windows.Forms.Padding(2);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(50, 25);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(89, 226);
+            this.button10.Margin = new System.Windows.Forms.Padding(2);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(100, 25);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "0";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comma
+            // 
+            this.comma.Location = new System.Drawing.Point(197, 226);
+            this.comma.Margin = new System.Windows.Forms.Padding(2);
+            this.comma.Name = "comma";
+            this.comma.Size = new System.Drawing.Size(50, 25);
+            this.comma.TabIndex = 15;
+            this.comma.Text = ".";
+            this.comma.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.comma.UseVisualStyleBackColor = true;
+            this.comma.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // back
+            // 
+            this.back.Location = new System.Drawing.Point(251, 191);
+            this.back.Margin = new System.Windows.Forms.Padding(2);
+            this.back.Name = "back";
+            this.back.Size = new System.Drawing.Size(31, 60);
+            this.back.TabIndex = 16;
+            this.back.Text = "<-";
+            this.back.UseVisualStyleBackColor = true;
+            this.back.Click += new System.EventHandler(this.back_Click);
+            // 
+            // deleteAll
+            // 
+            this.deleteAll.Location = new System.Drawing.Point(251, 118);
+            this.deleteAll.Margin = new System.Windows.Forms.Padding(2);
+            this.deleteAll.Name = "deleteAll";
+            this.deleteAll.Size = new System.Drawing.Size(31, 60);
+            this.deleteAll.TabIndex = 17;
+            this.deleteAll.Text = "C";
+            this.deleteAll.UseVisualStyleBackColor = true;
+            this.deleteAll.Click += new System.EventHandler(this.deleteAll_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "£GBP  ",
+            "¥JPY",
+            "$USD",
+            "₽руб"});
+            this.comboBox1.Location = new System.Drawing.Point(379, 74);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(107, 21);
+            this.comboBox1.TabIndex = 18;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // newCoin
+            // 
+            this.newCoin.Location = new System.Drawing.Point(432, 99);
+            this.newCoin.Margin = new System.Windows.Forms.Padding(2);
+            this.newCoin.Name = "newCoin";
+            this.newCoin.Size = new System.Drawing.Size(25, 25);
+            this.newCoin.TabIndex = 19;
+            this.newCoin.Text = "+";
+            this.newCoin.UseVisualStyleBackColor = true;
+            this.newCoin.Click += new System.EventHandler(this.newCoin_Click);
+            // 
+            // deleteCoin
+            // 
+            this.deleteCoin.Location = new System.Drawing.Point(461, 99);
+            this.deleteCoin.Margin = new System.Windows.Forms.Padding(2);
+            this.deleteCoin.Name = "deleteCoin";
+            this.deleteCoin.Size = new System.Drawing.Size(25, 25);
+            this.deleteCoin.TabIndex = 20;
+            this.deleteCoin.Text = "-";
+            this.deleteCoin.UseVisualStyleBackColor = true;
+            this.deleteCoin.Click += new System.EventHandler(this.deleteCoin_Click);
+            // 
+            // Convert
+            // 
+            this.Convert.Location = new System.Drawing.Point(305, 72);
+            this.Convert.Margin = new System.Windows.Forms.Padding(2);
+            this.Convert.Name = "Convert";
+            this.Convert.Size = new System.Drawing.Size(50, 25);
+            this.Convert.TabIndex = 21;
+            this.Convert.Text = "Convert";
+            this.Convert.UseVisualStyleBackColor = true;
+            this.Convert.Click += new System.EventHandler(this.Convert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(568, 350);
+            this.Controls.Add(this.Convert);
+            this.Controls.Add(this.deleteCoin);
+            this.Controls.Add(this.newCoin);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.deleteAll);
+            this.Controls.Add(this.back);
+            this.Controls.Add(this.comma);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button comma;
+        private System.Windows.Forms.Button back;
+        private System.Windows.Forms.Button deleteAll;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button newCoin;
+        private System.Windows.Forms.Button deleteCoin;
+        private System.Windows.Forms.Button Convert;
+    }
+}
+
---
+++ ComponentBasic2_AitorGallardo/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComponentBasic2_AitorGallardo
+{
+    public partial class Form1 : Form
+    {
+        List<double> coinsValue = new List<double>();
+        public Form1()
+        {
+            InitializeComponent();
+            for(int i =0; i < 4; i++)
+            {
+                this.coinsValue.Add(0);
+            }           
+        }
+
+        private void button2_Click(object sender, EventArgs e) //Adds 0-9 numbers to currentOne
+        {
+            // textBox1.Text.Substring(0).Equals("0");
+            Button button = (Button)sender;
+            if (textBox1.Text.Contains("."))
+            {
+                int commaPos = textBox1.Text.IndexOf(".");
+                if (commaPos == 0 && button.Text != ".")
+                {
+                    textBox1.Text = "0" + textBox1.Text + button.Text;
+                }
+                else if (button.Text == ".")
+                {
+                    textBox1.Text = textBox1.Text;
+                }
+                else if (textBox1.Text.Length == commaPos || textBox1.Text.Length == commaPos + 2 || textBox1.Text.Length == commaPos + 1)
+                {
+                    textBox1.Text += button.Text;
+                }
+            }
+            else if (textBox1.Text.IndexOf("0") == 0 && textBox1.Text.Length == 1 && button.Text != ".")
+            {
+
+                textBox1.Text = button.Text;
+            }
+            else
+            {
+                textBox1.Text += button.Text;
+            }
+        }
+
+        private void deleteAll_Click(object sender, EventArgs e)// Set textBox to 0
+        {
+            textBox1.Text = "0";
+        }
+
+        private void back_Click(object sender, EventArgs e) // Detelete last number
+        {
+            if(textBox1.Text.Length > 0)
+            textBox1.Text = textBox1.Text.Substring(0, textBox1.Text.Length - 1);
+        }
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void newCoin_Click(object sender, EventArgs e)
+        {
+            String valor = Microsoft.VisualBasic.Interaction.InputBox("Add a new currency");
+            comboBox1.Items.Add(valor);
+        }
+
+        private void deleteCoin_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                int index = comboBox1.Items.IndexOf(comboBox1.SelectedItem);
+                comboBox1.Items.Remove(comboBox1.SelectedItem);
+                this.coinsValue.RemoveAt(index);
+            }
+
+        }
+
+        private void Convert_Click(object sender, EventArgs e)
+        {
+
+            if (comboBox1.SelectedItem != null && textBox1.Text.Length > 0)
+            {
+                int index = comboBox1.Items.IndexOf(comboBox1.SelectedItem);
+
+                if (this.coinsValue[index] == 0)
+                {
+                    String currencyValue = Microsoft.VisualBasic.Interaction.InputBox("Introduce ratio convertion. Currency value per 1€");
+
+                    if (currencyValue.Length > 0)
+                    {
+                        try
+                        {
+                            this.coinsValue[index] = Double.Parse(currencyValue);
+                            textBox1.Text = (Double.Parse(textBox1.Text) * this.coinsValue[index]).ToString();
+                        }
+                        catch(Exception exception)
+                        {
+                            MessageBox.Show("Invalid input");
+                        }
+
+                    }
+                }
+                else
+                {
+                    this.coinsValue[index] = this.coinsValue[index];
+                    textBox1.Text = (Math.Round(Double.Parse(textBox1.Text) * this.coinsValue[index], 2)).ToString();
+                }
+            }
+            else if (comboBox1.SelectedItem == null)
+            {
+                MessageBox.Show("Error! Select a currency to convert to");
+            }
+            else
+            {
+                MessageBox.Show("Error! Insert a value to convert");
+            }
+
+        }
+
+    }
+}
